1de1110429a6ac3cf386cbf0bd9fe77a7fa0e89f
Eugene Polevikov <eugene.polevikov@yandex.ru>
2024-01-06 15:50:20 +0300

    Implement redrawing line on drag move

--- src/components/Line/Line.js
+++ src/components/Line/Line.js
@@ -1,6 +1,11 @@
-import { Circle } from 'react-konva';
+import { Circle, Line as KonvaLine } from 'react-konva';
+import { useState } from 'react';
 
 export function Line({ line, setLine, stageWidth, stageHeight, gridIndent }) {
+  const [konvaLinePoints, setKonvaLinePoints] = useState([
+    line[0].x, line[0].y, line[1].x, line[1].y
+  ]);
+
   function handleDragEnd(event) {
     let dragEndX = event.target.x();
     let dragEndY = event.target.y();
@@ -17,7 +22,7 @@ export function Line({ line, setLine, stageWidth, stageHeight, gridIndent }) {
 
       let sndPointId = (pointId + 1) % 2;
 
-      if (finalX == line[sndPointId].x && finalY == line[sndPointId].y) {
+      if (finalX === line[sndPointId].x && finalY === line[sndPointId].y) {
         finalX = line[pointId].x;
         finalY = line[pointId].y;
       }
@@ -37,6 +42,26 @@ export function Line({ line, setLine, stageWidth, stageHeight, gridIndent }) {
 
       return point;
     }));
+
+    let newKonvaLinePoints = JSON.parse(JSON.stringify(konvaLinePoints));
+
+    newKonvaLinePoints[2 * pointId] = finalX;
+    newKonvaLinePoints[2 * pointId + 1] = finalY;
+
+    setKonvaLinePoints(newKonvaLinePoints);
+  }
+
+  function handleDragMove(event) {
+    let x = event.target.x();
+    let y = event.target.y();
+    let targetId = Number(event.target.id());
+
+    let newKonvaLinePoints = JSON.parse(JSON.stringify(konvaLinePoints));
+
+    newKonvaLinePoints[2 * targetId] = x;
+    newKonvaLinePoints[2 * targetId + 1] = y;
+
+    setKonvaLinePoints(newKonvaLinePoints);
   }
 
   return (
@@ -50,9 +75,17 @@ export function Line({ line, setLine, stageWidth, stageHeight, gridIndent }) {
           radius={gridIndent / 4}
           fill={'black'}
           draggable
+          onDragMove={handleDragMove}
           onDragEnd={handleDragEnd}
         />
       ))}
+      <KonvaLine
+        id={"0"}
+        key={"0"}
+        points={konvaLinePoints}
+        stroke={'black'}
+        strokeWidth={1}
+      />
     </>
   );
 }
