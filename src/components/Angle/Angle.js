283af716ac3e8c0d0379dcba769aaab4321cafdb
Eugene Polevikov <eugene.polevikov@yandex.ru>
2024-01-12 16:41:17 +0300

    Refactoring

--- src/components/Angle/Angle.js
+++ src/components/Angle/Angle.js
@@ -1,54 +1,7 @@
-import { Circle, Arc, Text, Arrow, Line as KonvaLine } from 'react-konva';
+import { Circle, Text, Line as KonvaLine } from 'react-konva';
 import { useState } from 'react';
 
-function getAngle(anglePoints) {
-  let point1 = anglePoints[0];
-  let point2 = anglePoints[1];
-  let point3 = anglePoints[2];
-
-  let vector1 = {
-    x: point1.x - point2.x,
-    y: point1.y - point2.y
-  };
-
-  let vector2 = {
-    x: point3.x - point2.x,
-    y: point3.y - point2.y
-  };
-
-  let dotProduct = vector1.x * vector2.x + vector1.y * vector2.y;
-  let length1 = Math.sqrt(Math.pow(vector1.x, 2) + Math.pow(vector1.y, 2));
-  let length2 = Math.sqrt(Math.pow(vector2.x, 2) + Math.pow(vector2.y, 2));
-
-  let angle = Math.acos(dotProduct / length1 / length2);
-
-  return angle * 180 / Math.PI;
-}
-
-/*
-function getRotation(anglePoints, gridIndent) {
-  let point = {
-    x: anglePoints[1].x + gridIndent,
-    y: anglePoints[1].y
-  };
-
-  let rotation1 = getAngle([
-    point,
-    anglePoints[1],
-    anglePoints[0]
-  ]);
-
-  let rotation2 = getAngle([
-    point,
-    anglePoints[1],
-    anglePoints[2]
-  ]);
-
-  let rotation = Math.min(rotation1, rotation2);
-
-  return rotation;
-}
-*/
+import { getAngle } from '../../services/Geometry';
 
 function getKonvaLinePoints(anglePoints) {
   return [
@@ -70,7 +23,6 @@ export function Angle({
   );
 
   const [angle, setAngle] = useState(getAngle(anglePoints));
-  // const [rotation, setRotation] = useState(getRotation(anglePoints, gridIndent));
 
   function handleDragEnd(event) {
     let dragEndX = event.target.x();
@@ -117,7 +69,6 @@ export function Angle({
     setAnglePoints(newAnglePoints);
     setKonvaLinePoints(getKonvaLinePoints(newAnglePoints));
     setAngle(getAngle(newAnglePoints));
-    // setRotation(getRotation(newAnglePoints, gridIndent));
   }
 
   function handleDragMove(event) {
@@ -132,7 +83,6 @@ export function Angle({
 
     setKonvaLinePoints(getKonvaLinePoints(updatedAnglePoints));
     setAngle(getAngle(updatedAnglePoints));
-    // setRotation(getRotation(updatedAnglePoints, gridIndent));
   }
 
   return (
@@ -184,28 +134,3 @@ export function Angle({
     </>
   );
 }
-
-/*
-  <Arc
-    x={konvaLinePoints[2]}
-    y={konvaLinePoints[3]}
-    innerRadius={gridIndent / 4}
-    outerRadius={gridIndent}
-    angle={angle}
-    fill={'blue'}
-    opacity={0.25}
-    stroke={'black'}
-    strokeWidth={1}
-  />
-  <Arrow
-    x={gridIndent}
-    y={gridIndent}
-    points={[0, 0, 4 * gridIndent, 0, 4 * gridIndent, 4 * gridIndent]}
-    pointerLength={gridIndent}
-    pointerWidth={gridIndent}
-    fill={'black'}
-    stroke={'black'}
-    strokeWidth={1}
-    tension={0.5}
-  />
-*/
--- src/hooks/FigureReducer.js
+++ src/hooks/FigureReducer.js
@@ -1,71 +1,7 @@
 import { ACTIONS } from "../constants/Action";
 import { TRANSFORMATIONS } from "../constants/Transformations";
 
-function reflectPoint(point, linePoints) {
-  let x1 = linePoints[0].x;
-  let y1 = linePoints[0].y;
-  let x2 = linePoints[1].x;
-  let y2 = linePoints[1].y;
-  
-  let k1 = (y2 - y1) / (x2 - x1);
-  let b1 = y1 - k1 * x1;
-  
-  // Coefficients of perpendicular line
-  let k2 = -1 / k1;
-  let b2 = point.y - k2 * point.x;
-
-  // Coordinates of lines intersection (initial and perpendicular)
-  let x = (b2 - b1) / (k1 - k2);
-  let y = k1 * x + b1;
-
-  // Coordinates of reflected point
-  let xReflected = point.x + 2 * (x - point.x);
-  let yReflected = point.y + 2 * (y - point.y);
-
-  return [xReflected, yReflected];
-}
-
-function reflectPoints(points, linePoints) {
-  let reflectedPoints = JSON.parse(JSON.stringify(points));
-
-  let x1 = linePoints[0].x;
-  let y1 = linePoints[0].y;
-  let x2 = linePoints[1].x;
-  let y2 = linePoints[1].y;
-
-  if (x1 === x2) {
-    for (let i = 0; i < reflectedPoints.length; i += 2) {
-      reflectedPoints[i] += 2 * (x1 - reflectedPoints[i]);
-    }
-
-    return reflectedPoints;
-  }
-
-  if (y1 === y2) {
-    for (let i = 1; i < reflectedPoints.length; i += 2) {
-      reflectedPoints[i] += 2 * (y1 - reflectedPoints[i]);
-    }
-
-    return reflectedPoints;
-  }
-
-  for (let i = 0; i < reflectedPoints.length; i += 2) {
-    let [xReflected, yReflected] = reflectPoint(
-      {x: reflectedPoints[i], y: reflectedPoints[i + 1]},
-      linePoints
-    );
-
-    reflectedPoints[i] = xReflected;
-    reflectedPoints[i + 1] = yReflected;
-  }
-
-  return reflectedPoints;
-}
-
-function rotatePoints(points, point, angle) {
-  let rotatedPoints = JSON.parse(JSON.stringify(points));
-  return rotatedPoints;
-}
+import { reflectPoints, rotatePoints } from "../services/Geometry";
 
 export function figureReducer(figureState, action) {
   let updatedFigureState = JSON.parse(JSON.stringify(figureState));
