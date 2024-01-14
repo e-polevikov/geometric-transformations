2b14327098bd9d88c784ec97fb398982b4448c81
Eugene Polevikov <eugene.polevikov@yandex.ru>
2024-01-14 12:51:50 +0300

    Adjust stage width and height sizes

--- src/constants/GeomStage.js
+++ src/constants/GeomStage.js
@@ -1,3 +1,3 @@
-export const STAGE_WIDTH = window.innerWidth * 0.75;
-export const STAGE_HEIGHT = window.innerHeight * 0.95;
-export const GRID_INDENT = STAGE_WIDTH / 50;
+export const STAGE_WIDTH = 1440; //window.innerWidth * 0.75;
+export const STAGE_HEIGHT = 960; //window.innerHeight * 0.95;
+export const GRID_INDENT = STAGE_WIDTH / 60;
